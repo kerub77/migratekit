d46a6a6d1a8e4eb423f0711e3c5c681c0db745e5
MarioL <33160225+kerub77@users.noreply.github.com>
2025-02-04 17:26:13 +0100

    Update main.go
    cutoverCmd.Flags

--- main.go
+++ main.go
@@ -353,7 +353,10 @@ func init() {
 	cutoverCmd.Flags().StringSliceVar(&securityGroups, "security-groups", nil, "Openstack security groups, comma separated (e.g. '42c5a89e-4034-4f2a-adea-b33adc9614f4,6647122c-2d46-42f1-bb26-f38007730fdc')")
 
 	cutoverCmd.Flags().BoolVar(&enablev2v, "run-v2v", true, "Run virt2v-inplace on destination VM")
-
+        
+	cutoverCmd.Flags().StringVar(&availabilityZone, "availability-zone", "", "OpenStack availability zone for blockdevice & server")
+        cutoverCmd.MarkFlagRequired("availability-zone")
+	
 	rootCmd.AddCommand(migrateCmd)
 	rootCmd.AddCommand(cutoverCmd)
 }
